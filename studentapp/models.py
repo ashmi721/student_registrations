2f8d65c16056cfd650d63dcb694d4ec81f447a3e
ashmi721 <ashmitak721@gmail.com>
2023-10-22 19:38:37 +0545

    complete to create the models

--- studentapp/models.py
+++ studentapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name= models.CharField(max_length=20) 
+    last_name= models.CharField(max_length=40)
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    city = models.CharField(max_length=50)
+    age = models.IntegerField()
+    address=models.CharField(max_length=100)
+    contact=models.CharField(max_length=14)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
