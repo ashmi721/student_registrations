1d1c7b30718d3b056b27c656b4ac10e326e87264
ashmi721 <ashmitak721@gmail.com>
2023-10-23 21:03:12 +0545

    create the form of the student registerations

---
+++ studentapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+
+class RegisterForm(forms.Form):
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Enter First Name"}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Enter Last Name"}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Enter Username"}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={"class": "form-control", "placeholder": "Enter Email"}))
+    city = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Enter City"}))
+    address = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Enter Address"}))
+    contact = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Enter Contact"}))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(
+        attrs={"class": "form-control", "placeholder": "Enter Password"}))
+    conform_password = forms.CharField(max_length=100, widget=forms.PasswordInput(
+        attrs={"class": "form-control", "placeholder": "Repeat your Password"}))        
--- studentapp/models.py
+++ studentapp/models.py
@@ -12,3 +12,6 @@ class Student(models.Model):
     contact=models.CharField(max_length=14)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+def __str__(self):
+    return self.username
