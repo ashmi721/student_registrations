43d7195b50f6410e26fee680a8d61f5d7140f539
ashmi721 <ashmitak721@gmail.com>
2023-10-24 22:07:06 +0545

    modified register file

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home,user_register,user_login
 urlpatterns = [
     path("", home),
-    path("register/",user_register),
+    path("register/",user_register,name='register_page'),
    
     path("login/",user_login,name="login_page"),
     
